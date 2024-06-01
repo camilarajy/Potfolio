e908fb610556036bf0e5eb53e6884edc6ac598d9
Camila Ramírez Jiménez <camilarajy@gmail.com>
2024-05-31 23:31:43 -0600

    second version complete

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Mathematician 🤓 & website developer💻.')
+  .typeString('Mathematician & Website Developer.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
